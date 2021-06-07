418bcc9ddea98b87e1255599224bdf8b550e2f7a
Dell <parmgill59522@gmail.com>
2021-06-07 10:29:19 +0530

    week2_assignment_1

---
+++ C0796418_week2_assignment1/date.py
@@ -0,0 +1,8 @@
+from datetime import date
+today = date.today()
+current_date = today.strftime("%d/%m/%y")
+print("The current date is =", current_date)
+
+
+
+
---
+++ C0796418_week2_assignment1/radius_of_circle.py
@@ -0,0 +1,5 @@
+from math import pi
+radius = float(input ("Enter the radius of circle : "))
+print("The area of the circle " + str(radius) + " is: " + str(pi * radius**radius))
+
+
